efbfdf8cbac672c173f7f7b76024c76b505bb92b
Razin Aleksey <38260609+unqleshka@users.noreply.github.com>
2025-09-17 22:36:42 +0300

    Add files via upload

---
+++ analyze_logs.sh
@@ -0,0 +1,54 @@
+awk '
+END {
+o = "Отчет о логе веб-сервера"
+p = "========================"
+r = "Общее количество запросов:"
+system("echo " o " > report.txt")
+system("echo " p " >> report.txt")
+system("echo " r "      " NR " >> report.txt")
+}' access.log
+
+awk '
+{
+a[$1]++
+}
+END{
+pr = "                           "
+r = "Количество уникальнх IP-адресов:"
+print2 = length(a)
+system("echo " r "      " print2 " >> report.txt")
+system("echo " pr " >> report.txt")
+}' access.log
+
+awk '
+{
+if (match($6,"GET")) {get += 1}
+else if (match($6,"POST")) {post += 1}
+}
+END{
+pr = "                           "
+prr = "   "
+r = "Количество запросов по методам:"
+system("echo " r " >> report.txt")
+system("echo " prr "" get " GET  >> report.txt")
+system("echo " prr "" post " POST  >> report.txt")
+system("echo " pr " >> report.txt")
+}' access.log
+
+awk '
+{
+count[$7]++;
+max = count[$7]
+}
+END{
+r = "Самый популярный URL:    "
+for (value in count)
+if (count[value]==max) {
+system("echo " r " " count[value] " " value " >> report.txt")}
+
+}' access.log
+
+awk '
+BEGIN {
+print "Отчет сохранен в файл report.txt"
+}'
